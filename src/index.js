f7b10f012937391224232b14833336f5f6f35498
YIO_XIO <gekkoivan@gmail.com>
2023-06-26 17:34:13 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -31,3 +31,18 @@ function deleteText() {
 }
 
 typeWriter();
+
+
+
+const root = document.querySelector('.marquee');
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for (let i = 0; i < marqueeElementsDisplayed; i++) {
+    marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
+
+
+
